ab7bb947bf01f8ec8abc19f0e8eb3b97c87a4be6
Aditya Vardhan Merla <amerla@xinu01.cs.purdue.edu>
2020-04-15 12:04:24 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -123,7 +123,7 @@ void Server::handle(const Socket_t& sock) const {
   {
     while ( getline (fs,line) )
     {
-      cout << line << '\n';
+      // cout << line << '\n';
     }
     fs.close();
   }
