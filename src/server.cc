865acda7af9a14d260dc8387d7b5661dd3b62a5f
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-14 22:26:01 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -51,35 +51,35 @@ std::vector<Route_t> route_map = {
 };
 */
 
-// void Server::handle(const Socket_t& sock) const {
-//   HttpRequest request;
-//   // TODO: implement parsing HTTP requests
-//   // recommendation:
-//   parse_request( sock, &request);
-//   request.print();
-
-//   HttpResponse resp;
-//   // TODO: Make a response for the HTTP request
-//   resp.http_version = "HTTP/1.1";
-//   std::cout << resp.to_string() << std::endl;
-//   sock->write(resp.to_string());
-// }
-
 void Server::handle(const Socket_t& sock) const {
-  // Maybe parse the request and print it too
- 
+  HttpRequest request;
+  // TODO: implement parsing HTTP requests
+  // recommendation:
+  parse_request( sock, &request);
+  request.print();
+
   HttpResponse resp;
-  resp.http_version = "HTTP/1.2";
-  resp.status_code = 200;
-  resp.reason_phrase = "OK";
-  resp.headers["Connection"] = "close";
-  resp.headers["Content-Length"] = 12;
-  resp.message_body = "Hello CS252!";
- 
-  std::cout << "Start of HTTP response:" << std::endl;
-  std::cout << "{" << resp.to_string() << "}" << std::endl;
+  // TODO: Make a response for the HTTP request
+  resp.http_version = "HTTP/1.1";
+  std::cout << resp.to_string() << std::endl;
   sock->write(resp.to_string());
 }
+
+// void Server::handle(const Socket_t& sock) const {
+//   // Maybe parse the request and print it too
+ 
+//   HttpResponse resp;
+//   resp.http_version = "HTTP/1.2";
+//   resp.status_code = 200;
+//   resp.reason_phrase = "OK";
+//   resp.headers["Connection"] = "close";
+//   resp.headers["Content-Length"] = 12;
+//   resp.message_body = "Hello CS252!";
+ 
+//   std::cout << "Start of HTTP response:" << std::endl;
+//   std::cout << "{" << resp.to_string() << "}" << std::endl;
+//   sock->write(resp.to_string());
+// }
 
 
  void  parse_request(const Socket_t& sock, HttpRequest* const request){
