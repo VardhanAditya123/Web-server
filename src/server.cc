0a976726a51361300f4a26722a72b30b0b22bd60
Aditya Vardhan Merla <amerla@xinu01.cs.purdue.edu>
2020-04-15 11:59:59 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -132,7 +132,7 @@ void Server::handle(const Socket_t& sock) const {
     request-> http_version = vec.at(2);
     line = sock->readline();
     while(line!=NULL){
-      cout << sock << endl;
+      cout << line << endl;
     }
     // std::map<std::string, std::string> headers;
     // message_body;
