b9d260e483286d3805370eb903d028e19befa3b8
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-15 18:57:06 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -71,7 +71,7 @@ void Server::handle(const Socket_t& sock) const {
   resp.headers["Connection"] = "close";
   resp.headers["Content-Length"] = 12;
   resp.headers["Content-Type"] = "html";
-  resp.message_body = "CS 252 is a super shit class !";
+  resp.message_body = request.message_body;
  
   std::cout << resp.to_string() << std::endl; 
   sock->write(resp.to_string());
