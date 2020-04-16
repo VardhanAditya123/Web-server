fb239e4d26ea1c1b378c540e7dd810353437adf0
Aditya Vardhan Merla <amerla@xinu01.cs.purdue.edu>
2020-04-16 18:27:23 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -60,7 +60,7 @@ pthread_t thrID;
 pthread_attr_t attr;
 pthread_attr_init(&attr);
 pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-pthread_create(&thrID, &attr, (void* (*)(void*) )dispatchThread,(void *) threadParams);
+pthread_create(&thrID, &attr, (void* (*)(void*) )handle,(void *) threadParams);
 }
 }
 
