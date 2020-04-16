3692a3e2159f56bab024ae9355b228a361db9cf8
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-16 00:48:46 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -80,7 +80,7 @@ void Server::handle(const Socket_t& sock) const {
   resp.headers["Connection"] = "close";
   resp.headers["Content-Length"] = (request.message_body).length();
   resp.headers["Content-Type"] = "html";
-  if(request.headers["Authorization"].length()==0){
+  if((request.headers).find("Authorization") == (resp.headers).end()){
   resp.headers["WWW-Authenticate"] ="Basic realm=\"CS 252_web_server_p5 \"";
   resp.status_code = 401;
   }
