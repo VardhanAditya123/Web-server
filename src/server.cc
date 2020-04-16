044fc3da3dc441c033534563e50526aab5dcc579
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-16 00:35:42 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -67,11 +67,11 @@ void Server::handle(const Socket_t& sock) const {
   // TODO: Make a response for the HTTP request
   resp.http_version = request.http_version;
   resp.message_body = request.message_body;
-  resp.status_code = 200;
-  //  cout<< "DEBUGG " << resp.headers["Authorization"] << endl;
-  if ( (request.headers).find("Authorization") == (resp.headers).end() ) {
   resp.status_code = 401;
-  }
+  //  cout<< "DEBUGG " << resp.headers["Authorization"] << endl;
+  // if ( (request.headers).find("Authorization") == (resp.headers).end() ) {
+  // resp.status_code = 401;
+  // }
   else{
     if(resp.headers["Authorization"].compare("YWRpdHlhOnZhcmRoYW4K") == 0){
       resp.status_code = 200;
