fef624feb9832fd1a3359ce71221c7d44bdaa67e
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-16 14:57:19 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -35,6 +35,7 @@ void Server::run_linear() const {
 void Server::run_fork() const {
 while (1) {
 Socket_t slaveSocket = _acceptor.accept_connection();
+cout << slaveSocket->_socket << endl;
 if (slaveSocket >= 0) {
 int ret = fork();
  if (ret == 0) {
