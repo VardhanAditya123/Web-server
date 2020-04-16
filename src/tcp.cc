93e0069daf3597caf95cfcebbda0934527ead48c
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-16 15:24:48 -0400

    Commit

--- src/tcp.cc
+++ src/tcp.cc
@@ -123,7 +123,6 @@ Socket_t TCPSocketAcceptor::accept_connection() const {
         throw ConnectionError("Unable to accept connection: " + std::string(strerror(errno)));
     }
     cout << "DDEBUG  " << s << endl;
-    s_return = s;
     return std::make_unique<TCPSocket>(s, addr);
 }
 
