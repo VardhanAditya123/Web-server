48cacde7d0f883707f7f0a784263de547ecfc4cd
Aditya Vardhan Merla <amerla@xinu01.cs.purdue.edu>
2020-04-17 17:16:09 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -28,7 +28,6 @@ void separate(HttpRequest* const request , string line);
 Server::Server(SocketAcceptor const& acceptor) : _acceptor(acceptor) { }
 
 string filename;
-char * memblock;
 streampos size;
 int hflag=0;
 
