88bcdae7c971f14158dd6e66bbe7b5f3c136e201
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-17 12:13:29 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -87,7 +87,7 @@ void* Server::run_linear2(Server* serv) const {
   }
 }
 
-void* run_linear3() const {
+void* run_linear3()  {
   while (1) {
     Socket_t sock = _acceptor.accept_connection();
     handle(sock);
