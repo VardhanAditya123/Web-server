c0a44f8a280d0aad96eb0ad6ede5beed41cbbf86
Aditya Vardhan Merla <amerla@xinu01.cs.purdue.edu>
2020-04-17 19:23:15 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -252,7 +252,8 @@ void  parse_request(const Socket_t& sock, HttpRequest* const request){
         cout << "SIIZE: " << size << endl;
         buff = new char [size];
         file.seekg (0, ios::beg);
-        file.read (buff, size);
+        // file.read (buff, size);
+        file1.write(buff,size);
         cout << buff <<endl;
         file.close();
       }
