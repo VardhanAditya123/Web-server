4de94e125b23f50076f720f67e133fed7ec539e9
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-17 00:28:07 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -98,7 +98,7 @@ void Server::loopthread (const Socket_t& sock) const  {
 
 void Server::run_thread_pool(const int num_threads) const {
   pthread_t thread[num_threads];
-  Socket_t  master_socket = _acceptor.accept_connection();
+  Socket_t  master_socket ;
   for (int i=0; i<num_threads; i++) {
     
     pthread_attr_t attr;
