9ba943b4b95fff2f641e62eb82f19fd6bca84dd3
Aditya Vardhan Merla <amerla@xinu01.cs.purdue.edu>
2020-04-17 02:31:30 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -100,7 +100,7 @@ void Server::run_thread_pool(const int num_threads) const {
   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
   Socket_t master = _acceptor.accept_connection();
   for (int i=0; i<num_threads; i++) {
-    std::thread t(&Server::loopthread);  
+    thread[i]=std::thread t(&Server::loopthread,this);  
   } 
   loopthread ();
 }
