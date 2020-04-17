49e110f501118c605ce2b74e35440228afd978de
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-17 12:40:20 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -100,7 +100,7 @@ void Server::run_thread_pool(const int num_threads) const {
 
   for (int i=0; i<num_threads; i++) {
 
-    pthread_create(&thread[i],&attr,(void* (*)(void*))test,this);
+    pthread_create(&thread[i],&attr,(void* (*)(void*))test,NULL);
   } 
 }
 
