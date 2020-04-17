85f83edb2636bf50b05595ea0f7ff2b88e3313a5
Aditya Vardhan Merla <amerla@xinu01.cs.purdue.edu>
2020-04-17 17:18:49 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -258,7 +258,7 @@ void  parse_request(const Socket_t& sock, HttpRequest* const request){
 
     // copies all data into buffer
     char*memblock;
-    std::vector<unsigned char> memblock(std::istreambuf_iterator<char>(input), {});
+     memblock(std::istreambuf_iterator<char>(input), {});
      size = input.tellg();
      sock->write(memblock,size);
     }
