0e99c615d82fadddeec96b518ee552a2bbc532cd
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-17 01:24:41 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -80,7 +80,7 @@ void Server::run_thread() const {
 }
 
 
-void loopthread () const   {
+void loopthread ()   {
  while (1) {
   Socket_t sock = _acceptor.accept_connection();
   ThreadParams * threadParams = new ThreadParams;
