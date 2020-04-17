19ef09a71a2666e70f07f67986025f29de4f66ce
Aditya Vardhan Merla <amerla@xinu01.cs.purdue.edu>
2020-04-16 23:41:04 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -80,7 +80,7 @@ void Server::run_thread() const {
 }
 
 
-void Server::loopthread (const Socket_t& sock) {
+void Server::loopthread (const Socket_t& sock) const {
   while (1) {
       Server::handle(sock);
     }
