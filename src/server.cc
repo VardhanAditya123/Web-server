033d0592a070c64f8c0044bbf217cf4740fffc6f
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-18 18:16:30 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -299,7 +299,7 @@ void separate(HttpRequest* const request , string line){
     token = strtok(NULL, ":"); 
   }
   if(vec.size() !=2){
-    cout << "FINAL ERRRORR" <<  vec.size() <<endl;
+    cout << "FINAL ERRRORR " <<  vec.size() <<endl;
   }
   request->headers[vec.at(0)]=vec.at(1); 
 }
