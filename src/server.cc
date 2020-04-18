9d2f5dbd7ad5d499500311e8113793955fb99ae5
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-18 15:55:46 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -248,7 +248,7 @@ void  parse_request(const Socket_t& sock, HttpRequest* const request){
   }
     else{
       // streampos size;
-      cout  <<fn << endl;
+      // cout  <<fn << endl;
       ifstream file (fn, ios::in|ios::binary|ios::ate);
       if (file.is_open())
       {
@@ -258,7 +258,7 @@ void  parse_request(const Socket_t& sock, HttpRequest* const request){
        nstr="";
       int c;
       FILE *f = fopen(fn.c_str(), "rb");
-      cout << "SIIIZE: " << size <<endl;
+      // cout << "SIIIZE: " << size <<endl;
       for(int i = 0 ; i < size ; i++){
         c = fgetc(f);
         nstr+=c;
