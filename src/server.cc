22cbfb442318f369d8a72b2eb83c2c0dcae44b3e
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-18 19:39:59 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -201,13 +201,14 @@ void  parse_request(const Socket_t& sock, HttpRequest* const request){
   char lastChar = 0;
   string line = sock->readline();
   line=trim(line);
-  cout << line << endl;
+
   char *token = strtok((char*)(line.c_str()), " "); 
 
   // Keep printing tokens while one of the 
   // delimiters present in str[]. 
   char*arrr[3];
   int c1=0;
+  cout << line << endl;
   while (token != NULL) 
   { 
 
