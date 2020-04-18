124b9c19e4922e6d193a70d40d28d9751829e940
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-18 19:33:17 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -212,7 +212,7 @@ void  parse_request(const Socket_t& sock, HttpRequest* const request){
   { 
 
     vec.push_back(trim(token));
-    arrr[c++]=trim(token);
+    arrr[c1++]=trim(token);
     token = strtok(NULL, " "); 
 
   } 
