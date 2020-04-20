7760de12540c6a98c1e37a006a4a781f04d4d8db
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-20 14:45:36 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -203,11 +203,13 @@ void  parse_request(const Socket_t& sock, HttpRequest* const request){
     second+=ch;
   }
   i1=i1+1;
+  if(line.size!=0){}
   for( ; i1 < line.length()-2; i1++){
     char ch = line.at(i1);
     third+=ch;
   }
   third=trim(third);
+  }
   // cout << third << endl;
   std::fstream fs; 
 
