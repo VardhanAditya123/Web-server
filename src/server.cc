a84e2e25af6ead6a90e81ddd9f8cef42ae9b346a
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-20 22:48:05 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -160,10 +160,6 @@ void Server::handle(const Socket_t& sock) const {
     if(s.compare( "Basic YWRpdHlhOnZhcmRoYW4=") == 0){
       if(resp.message_body.length()!=0)
       resp.status_code=200; 
-      else{
-      resp.status_code=401;
-      resp.message_body="";
-      }
     }
     else{
       resp.status_code = 401;
