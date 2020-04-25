cae0a20c2c50259283e0f91c367c0bbf3ba9d4d3
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-25 15:35:58 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -27,9 +27,18 @@ using namespace std;
 HttpResponse handle_cgi_bin(const HttpRequest& request,vector <string> vec) {
   HttpResponse response;
   request.print();
+  string msg;
   cout << "HERERE" << endl;
   response.http_version = request.http_version;
   string fn = "http-root-dir"+vec.at(1);
+
+  std::ifstream is(fn);     // open file
+  char c;
+  while (is.get(c))          // loop getting single characters
+    msg+=c;
+
+  is.close();
+
   // TODO: Task 2.2
   return response;
 }
