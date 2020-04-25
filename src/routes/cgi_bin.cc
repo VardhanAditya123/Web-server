c8aacb37f3d9ef23e68e392cb6fa9d63181fb316
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-25 15:30:13 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -1,8 +1,30 @@
 #include "http_messages.hh"
+#include <functional>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <tuple>
+#include <unistd.h>
+#include <string.h>
+#include "server.hh"
+#include "http_messages.hh"
+#include "errors.hh"
+#include "misc.hh"
+#include "routes.hh"
+#include <cstring>
+#include <fstream>
+#include <pthread.h>
+#include <stdio.h>
+#include <thread>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <algorithm>
 using namespace std;
 // You could implement your logic for handling /cgi-bin requests here
 
-HttpResponse handle_cgi_bin(const HttpRequest& request,vector<string> vec) {
+HttpResponse handle_cgi_bin(const HttpRequest& request,vector <string> vec) {
   HttpResponse response;
   request.print();
   cout << "HERERE" << endl;
