6e5591156befc6f0932eb01c43725c49769afcce
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-25 17:13:29 -0400

    Commit

--- include/server.hh
+++ include/server.hh
@@ -16,7 +16,7 @@ class Server {
     void run_thread() const;
     void handle(const Socket_t& sock) const;
     void loopthread() const ;
-    void separate() const;
+    void separate(HttpRequest* const request , string line) const;
 };
 
 #endif  // INCLUDE_SERVER_HH_
