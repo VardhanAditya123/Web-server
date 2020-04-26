e250a0ab79c65cdbb6f32f216de91083769e43c9
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-26 15:07:50 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -65,7 +65,12 @@ HttpResponse handle_cgi_bin(const Socket_t& sock,HttpRequest* const request,vect
   int ret = fork();
 
   if (ret == 0) {
-    
+    dup2(out[1],1);
+    close(out[1]);
+    setenv( "REQUEST_METHOD","GET",1);
+    if((request->query).length()!=0)
+      setenv("QUERY_STRING",second3.c_str(),1);
+
     if(fn.find(".so")!=std::string::npos){
       void * lib = dlopen( "http-root-dir/cgi-bin/jj-mod.so", RTLD_LAZY );
 
@@ -85,14 +90,8 @@ HttpResponse handle_cgi_bin(const Socket_t& sock,HttpRequest* const request,vect
       hello_httprun( 1, second3.c_str());
     }
 
-    else{
-      dup2(out[1],1);
-    close(out[1]);
-    setenv( "REQUEST_METHOD","GET",1);
-    if((request->query).length()!=0)
-      setenv("QUERY_STRING",second3.c_str(),1);
+    else
       execl(fn.c_str(),NULL);
-      }
 
   }
 
