81d8cc4e34426093d67ebe6171edc857161ef86c
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-26 23:42:04 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -286,7 +286,7 @@ void handle_stat(const Socket_t& sock,HttpRequest* const request,vector <string>
   auto end = std::chrono::system_clock::now();
   std::chrono::duration<double> elapsed_seconds = end-start;
   msg+= "Aditya Vardhan\n" ;
-  msg+= "Number of Requests: "+req_count +std::string("\n");
+  msg+= "Number of Requests: " +std::string("\n");
   msg+= "Elapsed time: " + std::to_string(elapsed_seconds.count()) + "\n";
   cout << msg << endl;
   string first = vec.at(0);
