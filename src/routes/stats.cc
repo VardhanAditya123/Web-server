bb4139d0e168001a89dddd1dffbd71822d7401f8
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-26 21:25:35 -0400

    Commit

--- src/routes/stats.cc
+++ src/routes/stats.cc
@@ -1,6 +1,27 @@
 #include "http_messages.hh"
-
-// If you use the recommended route handling scheme, you will probably want a default handler
+#include <functional>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <tuple>
+#include <unistd.h>
+#include <string.h>
+#include "server.hh"
+#include "http_messages.hh"
+#include "errors.hh"
+#include "misc.hh"
+#include "routes.hh"
+#include <cstring>
+#include <fstream>
+#include <pthread.h>
+#include <stdio.h>
+#include <thread>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <algorithm>
+using namespace std;er
 
 void handle_stats(const HttpRequest& request) {
  
