e8b1c1ae199926a94c85e6ab33b54f335b6dd8f3
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-26 21:57:17 -0400

    Commit

--- include/server.hh
+++ include/server.hh
@@ -6,7 +6,6 @@
 class Server {
  private:
     SocketAcceptor const& _acceptor;
-    double min_time, max_time, start_time, min_time_URL, max_time_URL;
 
  public:
     explicit Server(SocketAcceptor const& acceptor);
@@ -17,6 +16,7 @@ class Server {
     void run_thread() const;
     void handle(const Socket_t& sock) const;
     void loopthread() const ;
+    double min_time, max_time, start_time, min_time_URL, max_time_URL;
 };
 
 #endif  // INCLUDE_SERVER_HH_
