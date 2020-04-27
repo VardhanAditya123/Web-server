2c4d6deeb97773e50fb91298011da7776fe3d958
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-26 23:11:01 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -232,7 +232,7 @@ void  parse_request(const Socket_t& sock, HttpRequest* const request){
     handle_cgi_bin(sock,request,vec);
   }
   else if(second.find("stats") != std::string::npos){
-    handle_stat(sock,request,vec);
+    handle_stat();
   }
   else{
     handle_htdocs(sock,request,vec);
