3f6a6d71e2e14a9ab0324e7183d544b5469ceb50
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-27 12:07:39 -0400

    Commit

--- src/routes/htdocs.cc
+++ src/routes/htdocs.cc
@@ -39,8 +39,9 @@ HttpResponse handle_htdocs(const Socket_t& sock,HttpRequest* const request,vecto
   string third = vec.at(2);
   string fn = "http-root-dir/htdocs"+second;
   cout << fn << endl;
+
  if(isDir(fn.c_str())==1){
-   
+    cout <<" YES SEY" << endl;
     if(second.at(second.length()-1 )== '/'){
      second+="index.html";
     }
