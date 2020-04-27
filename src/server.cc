96473bb44e53dae32e37d86a95fc2c6522731654
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-26 21:59:22 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -43,7 +43,7 @@ void Server::run_linear() const {
 }
 
 void Server::run_fork() const {
-  // Server:start_time = std::chrono::system_clock::now();
+  start_time = std::chrono::system_clock::now();
   while (1) {
     Socket_t slaveSocket = _acceptor.accept_connection();
     int ret = fork();
