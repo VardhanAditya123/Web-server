a472112faa7a0948d49aea0d40609ab29eff770f
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-27 10:45:13 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -66,7 +66,8 @@ void Server::run_fork() const {
       handle(slaveSocket);
       exit(0);
     }
-    cout << iti->it_value.tv_sec << endl;
+    timer_gettime(tid,iti);
+    cout << iti->it_interval.tv_sec << endl;
     waitpid(-1, NULL, WNOHANG) ;
 
 
