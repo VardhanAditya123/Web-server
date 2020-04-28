2cd3351f3fc31008fffa88f1e819cb4aa989c8c7
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-28 00:08:29 -0400

    Commit

--- src/tcp.cc
+++ src/tcp.cc
@@ -74,6 +74,9 @@ std::string TCPSocket::readline() {
 void TCPSocket::write(std::string const& str) {
     write(str.c_str(), str.length());
 }
+int TCPSocket::ret_sock(){
+    return _socket2;
+}
 
 void TCPSocket::write(char const * const buf, const size_t buf_len) {
     if (buf == NULL) return;
