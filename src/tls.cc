e9921b1dcad836bc6c19eefd0f5293b6f0809552
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-28 12:23:20 -0400

    Commit

--- src/tls.cc
+++ src/tls.cc
@@ -20,7 +20,10 @@
 
 #include "tls.hh"
 #include "errors.hh"
-
+SSL_CTX *create_context();
+void init_openssl();
+void configure_context(SSL_CTX *ctx);
+void cleanup_openssl();
 
 TLSSocket::TLSSocket(int port_no, struct sockaddr_in addr, SSL* ssl) :
   _socket(port_no), _addr(addr), _ssl(ssl) {
@@ -66,12 +69,61 @@ void TLSSocket::write(char const *const buf, const size_t buf_len) {
 
 TLSSocketAcceptor::TLSSocketAcceptor(const int portno) {
     // TODO: Task 2.1
+    //call all methods
+    //init , create , configure
 }
 
 Socket_t TLSSocketAcceptor::accept_connection() const {
     // TODO: Task 2.1
+    //call
+    //while in examples
+    //remodel to tcp
 }
 
 TLSSocketAcceptor::~TLSSocketAcceptor() noexcept {
     // TODO: Task 2.1
+}
+
+SSL_CTX *create_context()
+{
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    method = SSLv23_server_method();
+
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+	perror("Unable to create SSL context");
+	ERR_print_errors_fp(stderr);
+	exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
+
+void init_openssl()
+{ 
+    SSL_load_error_strings();	
+    OpenSSL_add_ssl_algorithms();
+}
+
+void cleanup_openssl()
+{
+    EVP_cleanup();
+}
+
+void configure_context(SSL_CTX *ctx)
+{
+    SSL_CTX_set_ecdh_auto(ctx, 1);
+
+    /* Set the key and cert */
+    if (SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+	exit(EXIT_FAILURE);
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM) <= 0 ) {
+        ERR_print_errors_fp(stderr);
+	exit(EXIT_FAILURE);
+    }
 }
