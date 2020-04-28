f778d16767805eeb19987393e2ba6e2d30bad8b3
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-28 12:32:12 -0400

    Commit

--- src/tls.cc
+++ src/tls.cc
@@ -85,7 +85,29 @@ Socket_t TLSSocketAcceptor::accept_connection() const {
 }
 
 TLSSocketAcceptor::~TLSSocketAcceptor() noexcept {
-    // TODO: Task 2.1
+     int s;
+    struct sockaddr_in addr;
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0) {
+	perror("Unable to create socket");
+	exit(EXIT_FAILURE);
+    }
+
+    if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+	perror("Unable to bind");
+	exit(EXIT_FAILURE);
+    }
+
+    if (listen(s, 1) < 0) {
+	perror("Unable to listen");
+	exit(EXIT_FAILURE);
+    }
+
 }
 
 SSL_CTX *create_context()
