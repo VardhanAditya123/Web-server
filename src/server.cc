f5ccef00b89b9544e26ae5ebe4da0b5a66f3556b
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-28 19:25:38 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -207,7 +207,6 @@ void Server::handle(const Socket_t& sock) const {
     resp.headers["Content-Type"] = get_content_type(request.filename);
   }
  
-  if(request.message_body.length()!=0)
   sock->write(resp.to_string());
 
 }
