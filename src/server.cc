d13cbd5b44263245caf8656077298e8ef5cab4e9
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-28 21:15:07 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -189,6 +189,9 @@ void Server::handle(const Socket_t& sock) const {
     }
   }
 
+    if( resp.status_code == 401 && request.message_body.length()==0){
+     return;
+   }
 
   resp.headers["Connection"] = "close";
   resp.headers["Content-Length"] = request.message_body.length();
@@ -206,9 +209,7 @@ void Server::handle(const Socket_t& sock) const {
     resp.headers["Content-Type"] = get_content_type(request.filename);
   }
    
-   if( resp.status_code == 401 && request.message_body.length()==0){
-     return;
-   }
+
   sock->write(resp.to_string());
 
 }
