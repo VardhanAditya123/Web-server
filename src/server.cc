851f387f0b08082c43aace0cef263bb96147714d
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-29 13:37:05 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -343,7 +343,7 @@ double findMax(){
 }
 
 
-void Server::update_stats() {
+void Server::update_stats()const{
   // auto end = std::chrono::system_clock::now();
   // std::chrono::duration<double> ns = end-start;
   // cout <<"NANO SEC " << std::to_string(ns.count()) << endl; 
