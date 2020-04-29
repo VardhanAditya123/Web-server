f71776c157b32000acde0be9849d4cb754a281b3
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-29 12:07:49 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -220,8 +220,8 @@ void Server::handle(const Socket_t& sock) const {
   sock->write(resp.to_string());
   auto end = std::chrono::system_clock::now();
   std::chrono::duration<double> ns = end-start;
-  cout <<"NANO SEC " << ns << endl; 
-  timer.push_back(ns);
+  // cout <<"NANO SEC " << ns << endl; 
+  timer.push_back(ns.count);
 
 
 }
