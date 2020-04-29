1f706f349369362ae621e3573baef9a0ef4d5644
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-29 13:24:24 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -41,7 +41,7 @@ auto start_server = std::chrono::system_clock::now();
 
 
 double findMax();
-void update_stats();
+// void update_stats();
 void  parse_request(const Socket_t& sock, HttpRequest* const request);
 void separate(HttpRequest* const request , string line);
 Server::Server(SocketAcceptor const& acceptor) : _acceptor(acceptor) { }
