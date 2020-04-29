d254f745709c69526af050741e7abe7033be318b
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-29 17:54:16 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -56,7 +56,6 @@ int req_count = 0;
 
 struct server_stats* s1 = mmap(NULL, FILESIZE, PROT_READ | PROT_WRITE,   MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
-struct server_stats s1;
 
 void Server::run_linear() const {
   while (1) {
