adcb96f2910ad70aa9f7a99120fee750dde2ad40
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-29 12:39:59 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -63,7 +63,7 @@ void Server::run_fork() const {
   
   while (1) {
     Socket_t slaveSocket = _acceptor.accept_connection();
-    req_count+=1;
+    // req_count+=1;
     timer_t tid;
     struct itimerspec * iti;
     int ret = fork();
@@ -223,6 +223,7 @@ void Server::handle(const Socket_t& sock) const {
   cout <<"NANO SEC " << std::to_string(ns.count()) << endl; 
   timer.push_back(ns.count());
   cout << timer.size()<< endl;
+  req_count+=1;
 
 
 }
