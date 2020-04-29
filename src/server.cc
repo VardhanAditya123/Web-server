a35f79348eb050aae8986e21cfd3eaf590d6b08c
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-29 19:02:47 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -58,7 +58,7 @@ double val;
 };
 
 struct server_stats* s1 = (server_stats*)mmap(NULL, 1000, PROT_READ | PROT_WRITE,   MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-s1->min=10;
+s1->min=10.0;
 
 
 void Server::run_linear() const {
