1eff668e0d8e60a88e81c40eab8459886cae6ce9
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-30 10:26:19 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -65,6 +65,7 @@ int p_no;
 // struct server_stats* s1 = (server_stats*)mmap(NULL, 1000, PROT_READ | PROT_WRITE,   MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 void Server::run_linear() const {
   s1.min = 10;
+  s1.p_no = port_number;
   while (1) {
     Socket_t sock = _acceptor.accept_connection();
     handle(sock);
