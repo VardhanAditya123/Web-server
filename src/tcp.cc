e1774064b9a602fd686bf4708e65ef36c9bd48be
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-30 00:46:37 -0400

    Commit

--- src/tcp.cc
+++ src/tcp.cc
@@ -97,6 +97,7 @@ void TCPSocket::write(char const * const buf, const size_t buf_len) {
 }
 
 TCPSocketAcceptor::TCPSocketAcceptor(const int portno) {
+    cout << port_no << endl;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(portno);
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
