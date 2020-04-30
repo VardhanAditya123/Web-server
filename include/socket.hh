d6bb577cafc78517e3a26d0d85bbb1c0b200189b
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-29 22:44:52 -0400

    Commit

--- include/socket.hh
+++ include/socket.hh
@@ -12,7 +12,7 @@ class Socket {
     virtual std::string readline();
     virtual void write(std::string const&) = 0;
     virtual void write(char const * const buf, size_t buf_len) = 0;
-    int get_portno();
+    virtual int get_portno();
 
 };
 
