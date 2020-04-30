a081e92d2c76a2b2d2d65aa4e5215bc20ad0b23b
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-04-30 00:38:10 -0400

    Commit

--- include/socket.hh
+++ include/socket.hh
@@ -21,7 +21,7 @@ typedef std::unique_ptr<Socket> Socket_t;
 class SocketAcceptor {
  public:
     virtual Socket_t accept_connection() const = 0;
-    int port_number = 78900;
+    virtual int port_number ;
 };
 
 #endif  // INCLUDE_SOCKET_HH_
