8751afc5098d0dac7bd5861e96de5a3c5d4182cd
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-05-01 14:03:25 -0400

    Commit

--- src/routes/htdocs.cc
+++ src/routes/htdocs.cc
@@ -83,7 +83,7 @@ int isDir(const char *path)
   return 0;
 }
 
-void generate_html(string& msg ,string& fn,HttpRequest* const){
+void generate_html(string& msg ,string& fn,HttpRequest* const request){
   string name;
   string link;
   DIR * d = opendir(fn.c_str());
