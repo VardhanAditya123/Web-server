7c6b78fb275049dee652ebc04ec663d732dd6177
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-05-01 16:46:10 -0400

    Commit

--- include/http_messages.hh
+++ include/http_messages.hh
@@ -16,7 +16,6 @@ class HttpRequest {
   std::string query;  // implemented in Task 2.2
   std::string filename;
   std::string content_type;
-  std::string null_check;
   int code;
   void print() const;
 };
