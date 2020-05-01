d567c5f124dc48f8c2d544072c1fb155cb7c34fa
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-05-01 16:21:36 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -279,7 +279,7 @@ void  parse_request(const Socket_t& sock, HttpRequest* const request){
   }
  
 
-  if (second.find("cgi-bin/jj") != std::string::npos) {
+  if (second.find("cgi-bin") != std::string::npos) {
     handle_cgi_bin(sock,request,vec);
   }
   else if(second.find("stats") != std::string::npos){
